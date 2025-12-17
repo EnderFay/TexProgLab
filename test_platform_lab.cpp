ff922474201e15cd691db7d888189673e741ef38
BillyMarkII <kur.nick2016@gmail.com>
2025-12-17 21:48:35 +0300

    Update test_platform_lab.cpp

--- test_platform_lab.cpp
+++ test_platform_lab.cpp
@@ -9,6 +9,6 @@ int main(){
   std::cout << "INVALID_SOCKET = "<< INVALID_SOCKET_TYPE << "\n";
 #endif
 
-  std::cout << "Размер сокета"<< sizeof(SOCKET_TYPE) << "байт\n";
+  std::cout << "Размер сокета "<< sizeof(SOCKET_TYPE) << " байт\n";
   return 0;
 }
